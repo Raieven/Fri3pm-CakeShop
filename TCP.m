6646329825a9c0b03126c16a45b999fc8803ed5f
AhemOne <36661387+AhemOne@users.noreply.github.com>
2019-08-23 13:33:32 +1000

    Create TCP.m

---
+++ TCP.m
@@ -0,0 +1,75 @@
+% Author: 
+% ID: 
+% Date: 
+% Description: 
+% MTRN4230 Group Assignment
+% Computer Vision program for ink printing 
+%
+% Objectification by Jesse Falzon
+%
+% Usage:
+%       make object with:
+%   >> Dec = Decoration();
+%
+%       call vision code with
+%   >> Image = imread('path/to.jpg');
+%   >> [from, to, angle] = Dec.update(Image);
+%
+%  Input: 
+%   Image - Image taken from table camera
+%   eg: Image = '1.png';
+%
+%  Output:
+%
+%   from(i,:)   - X, Y, Z coords of the i-th block on the conveyor
+%   to(i,:)     - X, Y, Z coords of the i-th block on the table
+%   angle(i)    - the angle to turn i-th block on the table
+
+
+function [tcpip_copy] = TCP()
+    global tcpip_internal;
+    
+    tcpip_internal.open = @openSocket;
+    tcpip_internal.close = @your_close_function;
+    tcpip_internal.receive = @your_receive_function;
+    tcpip_internal.send_decorations = @your_send_decorations_function;
+    tcpip_internal.send_ink = @your_send_ink_function;
+    tcpip_internal.send_toggle = @your_send_toggle_function;
+    tcpip_copy = tcpip_internal;
+end
+
+function openSocket(ip, port)
+    global tcpip_internal;
+% open socket to robot
+
+    tcpip_internal = tcpip(ip, port, 'NetworkRole', 'client');
+    fopen(tcpip_internal);
+
+end
+
+function [str] = your_close_function()
+    global tcpip_internal;
+% close socket to robot
+    fclose(tcpip_internal.socket);
+end
+
+function [str] = your_update_function()
+    global tcpip_internal;
+% recv error data from robot
+    str = [];
+end
+
+function your_send_decorations_function(data)
+    global tcpip_internal;
+% send decoration data to robot
+
+end
+
+function your_send_ink_function(data)
+    global tcpip_internal;
+% send ink data to robot
+
+end
+
+% ----------------- Function ------------------ %
+% INTERNAL FUNCTIONS HERE
