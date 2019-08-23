997377257b5cc02a5894c3b555647a7bb33f0fa3
AhemOne <36661387+AhemOne@users.noreply.github.com>
2019-08-23 14:15:13 +1000

    Update main.m

--- main.m
+++ main.m
@@ -1,26 +1,89 @@
 function main()
     global running;
     running = 0;
+    
+    global default_table_image default_conveyor_image
+    % setup table camera
+    [table_camera table_camera_source] = connect_to_camera(1);
+    table_camera_parameters = set_camera_parameters(table_camera_source, []);
+    table_camera_parameters(1) = -4;
+    set_camera_parameters(table_camera_source, table_camera_parameters);
+    default_table_image = imread('3.jpg');
+    
+    % setup conveyor camera
+    [conveyor_camera, conveyor_camera_source] = connect_to_camera(2);
+    conveyor_camera_parameters = set_camera_parameters(conveyor_camera_source, []);
+    conveyor_camera_parameters(1) = -6;
+    set_camera_parameters(conveyor_camera_source, conveyor_camera_parameters);
+    default_conveyor_image = imread('7.jpg');
+    
+    % set up TCP, ink and decoration objects
+    global TcpIp ink decoration;
+    TcpIp = TCP;
+    ink = InkPrinting;
+    decoration = Decoration;
+    
+    TcpIp.open('192.168.0.1', 5000);
 
+    % create main figure
     main_window = figure;
     main_window.CloseRequestFcn = @main_window_close;
 
+    % add control panel
     controls = uipanel('Parent', main_window);
     controls.Title = 'Options';
     controls.Position = [ 0 0 0.2 1 ];
 
+    % make quit button
     quit_button = uicontrol('Parent', controls);
     quit_button.String = 'Quit';
     quit_button.Callback = @quit_button_down;
     quit_button.Units = 'normalized';
     quit_button.Position = [ 0.05 0.05 0.9 0.1 ];
 
+    % make maintenance button
     maintenance_button = uicontrol('Parent', controls);
     maintenance_button.String = 'Maintenance';
     maintenance_button.Callback = @maintenance_button_down;
     maintenance_button.Units = 'normalized';
     maintenance_button.Position = [ 0.05 0.2 0.9 0.1 ];
+    
+    % make stop button
+    stop_button = uicontrol('Parent', controls);
+    stop_button.String = 'Stop';
+    stop_button.Callback = @stop_button_down;
+    stop_button.Units = 'normalized';
+    stop_button.Position = [ 0.05 0.35 0.9 0.1 ];
+    
+    % make pause button
+    pause_button = uicontrol('Parent', controls);
+    pause_button.String = 'Pause';
+    pause_button.Callback = @toggle_button_down;
+    pause_button.Units = 'normalized';
+    pause_button.Position = [ 0.05 0.5 0.9 0.1 ];
+    
+    % make Print ink button
+    ink_button = uicontrol('Parent', controls);
+    ink_button.String = 'Print Ink';
+    ink_button.Callback = @ink_button_down;
+    ink_button.Units = 'normalized';
+    ink_button.Position = [ 0.05 0.65 0.9 0.1 ];
+    
+    % make Print decorations button
+    decorations_button = uicontrol('Parent', controls);
+    decorations_button.String = 'Print Decorations';
+    decorations_button.Callback = @decoration_button_down;
+    decorations_button.Units = 'normalized';
+    decorations_button.Position = [ 0.05 0.8 0.9 0.1 ];
+    
+    % make error text panel button
+    error_text = uicontrol('Parent', controls);
+    error_text.Style = 'text';
+    error_text.String = 'Connecting';
+    error_text.Units = 'normalized';
+    error_text.Position = [ 0.05 0.9 0.9 0.10 ];
 
+    % create panel for output plots
     plots = uipanel('Parent', main_window);
     plots.Title = 'Feedback';
     plots.Position = [ 0.2 0 0.8 1 ];
@@ -28,35 +91,26 @@ function main()
     feedback_ax = axes('Parent', plots);
     feedback_ax.Position = [ 0 0 1 1 ];
 
+    % prepare axes for camera images
     table_cam_ax = subplot(2, 2, 1, feedback_ax);
     conveyor_cam_ax = subplot(2, 2, 2);
     table_out_ax = subplot(2, 2, 3);
     conveyor_out_ax = subplot(2, 2, 4);
     
-    [table_camera table_camera_source] = connect_to_camera(1);
-    table_camera_parameters = set_camera_parameters(table_camera_source, []);
-    table_camera_parameters(1) = -4;
-    set_camera_parameters(table_camera_source, table_camera_parameters);
-    default_table_image = imread('3.jpg');
+    global table_image;
+    % draw table camera
     table_image = get_image(table_camera, default_table_image);
     table_imshow = imshow(table_image, 'Parent', table_cam_ax);
     
-    [conveyor_camera, conveyor_camera_source] = connect_to_camera(2);
-    conveyor_camera_parameters = set_camera_parameters(conveyor_camera_source, []);
-    conveyor_camera_parameters(1) = -6;
-    set_camera_parameters(conveyor_camera_source, conveyor_camera_parameters);
-    default_conveyor_image = imread('7.jpg');
+    global conveyor_image;
+    % draw conveyor camera
     conveyor_image = get_image(conveyor_camera, default_conveyor_image);
     conveyor_imshow = imshow(conveyor_image, 'Parent', conveyor_cam_ax);
     
+    
     table_out_imshow = imshow(table_image, 'Parent', table_out_ax);
     
-    
-    % set up ink and decoration objects
-    ink = InkPrinting;
-    decoration = Decoration;
-    
-    [ink_out, Traj] = ink.update(table_image);
+    [ink_out, Traj, aj] = ink.update(table_image);
     [normal, bold] = parse_letters(ink_out);
     
     [a, b, theta] = decoration.update(table_image, conveyor_image);
@@ -80,11 +134,10 @@ function main()
         [ink_out, Traj] = ink.update(table_image);
         [normal, bold] = parse_letters(ink_out);
         
-        
-    ink_bold.XData = bold.x(:);
-    ink_bold.YData = bold.y(:);
-    ink_normal.XData = normal.x(:);
-    ink_normal.YData = normal.y(:);
+        ink_bold.XData = bold.x(:);
+        ink_bold.YData = bold.y(:);
+        ink_normal.XData = normal.x(:);
+        ink_normal.YData = normal.y(:);
         
         drawnow limitrate;
         pause(0.05);
@@ -107,6 +160,8 @@ function quit_button_down(fig,~)
 end
 
 function maintenance_button_down(main_fig,~)
+    global ink decoration;
+    
     maintenance_fig = figure;
     maintenance_fig.CloseRequestFcn = @maintenance_close;
     maintenance_fig.UserData = main_fig;
@@ -116,14 +171,14 @@ function maintenance_button_down(main_fig,~)
     maintenance_controls.Position = [ 0 0 0.2 1 ];
 
     vision_test = uicontrol('Parent', maintenance_controls);
-    vision_test.String = 'Unit test vision';
-    % vision_test.Callback = @vision_test;
+    vision_test.String = 'Unit test Decoration';
+    vision_test.Callback = @decoration.test;
     vision_test.Units = 'normalized';
     vision_test.Position = [ 0.05 0.05 0.9 0.1 ];
 
     motion_test = uicontrol('Parent', maintenance_controls);
-    motion_test.String = 'Unit test motion';
-    % motion_test.Callback = @motion_test;
+    motion_test.String = 'Unit test Inking';
+    motion_test.Callback = @ink.test;
     motion_test.Units = 'normalized';
     motion_test.Position = [ 0.05 0.2 0.9 0.1 ];
     
@@ -131,8 +186,8 @@ function maintenance_button_down(main_fig,~)
 end
 
 function maintenance_close(fig,~)
-    main_fig = fig.UserData;
-    main_fig.Parent.Parent.Visible = 'on';
+    main_fig = fig.UserData.Parent.Parent;
+    main_fig.Visible = 'on';
     fig;
     closereq;
 end
@@ -192,5 +247,41 @@ for i = 1:length(letters)
         normal.y = [normal.y; letters(i).trajectory(:,2); inf];
     end
 end
+end
+
+function stop_button_down(~,~)
+    fprintf('stop robot\n');
+    % TcpIp.send_stop();
+end
+
+function toggle_button_down(button,~)
+    global TcpIp;
+    
+    if strcmp(button.String, 'Pause')
+        button.String = 'Running';
+    else
+        button.String = 'Pause';
+    end
+    
+    TcpIp.send_toggle();
+end
+
+function ink_button_down(~,~)
+    global TcpIp ink;
+    global table_image;
+    
+    [~, b, ~] = ink.update(table_image);
+    
+    TcpIp.send_ink(b);
+end
+
+function decoration_button_down(~,~)
+    global TcpIp decoration;
+    global table_image;
+    global conveyor_image;
+    
+    [a] = decoration.update(table_image, conveyor_image);
+    
+    TcpIp.send_decorations(a);
 end
 
